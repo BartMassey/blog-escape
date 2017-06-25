77a5ca4419cc8ab709db2ebb15febcea9de70a10
Bart Massey <bart@cs.pdx.edu>
2017-06-25 08:32:56 -0700

    moved from _mysql to MySQLdb

--- drupal-jailbreak.py
+++ drupal-jailbreak.py
@@ -5,11 +5,14 @@
 # portable formats.
 
 import MySQLdb
+import MySQLdb.cursors
+
 db = MySQLdb.connect(user="drupal_fob",
                      db="drupal_fob",
-                     read_default_file="~/.drupal_jailbreak.my.cnf")
+                     read_default_file="~/.drupal_jailbreak.my.cnf",
+                     cursorclass=MySQLdb.cursors.DictCursor)
 
 c = db.cursor()
-c.execute("SELECT nid FROM node")
-for nid, in c:
-    print(nid)
+c.execute("SELECT * FROM node")
+for node in c:
+    print(node)
