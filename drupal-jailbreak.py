38af3c9bececa7aacf685f3a76c2c7ed50c036d7
Bart Massey <bart@cs.pdx.edu>
2017-06-30 23:46:15 -0700

    extracted actual content from the database

--- drupal-jailbreak.py
+++ drupal-jailbreak.py
@@ -7,15 +7,26 @@
 # Pull the content out of a Drupal site into
 # portable formats.
 
+import sys
+
 import MySQLdb
 import MySQLdb.cursors
 
 db = MySQLdb.connect(user="drupal_fob",
                      db="drupal_fob",
-                     read_default_file="~/.drupal_jailbreak.my.cnf",
-                     cursorclass=MySQLdb.cursors.DictCursor)
+                     read_default_file="~/.drupal_jailbreak.my.cnf")
 
 c = db.cursor()
-c.execute("SELECT * FROM node")
-for node in c:
-    print(node)
+c.execute("""SELECT format, name FROM filter_format""")
+formats = {fformat: fname for fformat, fname in c}
+c.execute(
+    """SELECT node.nid, node.title, field_data_body.body_value,
+    field_data_body.body_format FROM
+    node JOIN field_data_body ON node.nid = field_data_body.entity_id WHERE
+    node.type = \"blog\"""")
+for nid, title, body, format in c:
+    with open("nodes/%d" % (nid,), "w") as node_file:
+        print("title: %s" % (title,), file=node_file)
+        print("format: %s" % (formats[format],), file=node_file)
+        print(file=node_file)
+        print(body, file=node_file)
