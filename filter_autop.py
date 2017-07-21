db4c3e42f0eb9eb99445157da66b92a52fd6a4fc
Bart Massey <bart@cs.pdx.edu>
2017-07-21 11:50:38 -0700

    made filter_autop run; still has minor bugs

--- filter_autop.py
+++ filter_autop.py
@@ -5,8 +5,9 @@
 # Independently written, but heavily inspired by Epstein's
 # http://greenash.net.au/thoughts/2010/05/an-autop-django-template-filter/
 
-# Can change this to just 're' if you don't want to
-# mess with a separate module for a slight speedup.
+import re
+# This provides memoized implementations of some re
+# functions to avoid recompiling on every run.
 from re_memo import *
 
 html_escape_table = {
@@ -29,7 +30,7 @@ def html_escape(text):
 # This is a deliberately literal translation, with as little
 # modification as possible to the original. Comments herein
 # are from the original source.
-def filter_autop(value):
+def filter_autop(text):
     """Convert line breaks into <p> and <br> in an intelligent fashion."""
 
     # All block level tags
@@ -41,20 +42,20 @@ def filter_autop(value):
     # messing up code. We look for matched pairs and allow
     # basic nesting. For example: "processed <pre> ignored
     # <script> ignored </script> ignored </pre> processed"
-    chunks = split('(<(?:!--.*?--|/?(?:pre|script|style|object)[^>]*)>)',
-                   text, flags=S|I)
+    chunks = re_split('(<(?:!--.*?--|/?(?:pre|script|style|object)[^>]*)>)',
+                   text, flags=re.S|re.I)
     ignore = False
     ignoretag = None
     output = ''
     for i, chunk in zip(range(len(chunks)), chunks):
         if i & 1 == 1:
             # Passthrough comments.
-            if chunk.startswith('<!--')
-                output.append(chunk)
+            if chunk.startswith('<!--'):
+                output += chunk
             else:
                 # Opening or closing tag?
                 open = chunk[1] != '/'
-                tag = split('[ >]', chunk[2 - int(open):], maxsplit=2)[0]
+                tag = re_split('[ >]', chunk[2 - int(open):], maxsplit=2)[0]
                 if ignore:
                     if open:
                         ignore = True
@@ -65,33 +66,33 @@ def filter_autop(value):
                     ignoretag = None
         elif not ignore:
             # just to make things a little easier, pad the end
-            chunk = sub('\n*$', '', chunk) + "\n\n"
-            chunk = sub('<br />\s*<br />', "\n\n", chunk)
+            chunk = re_sub(r'\n*$', '', chunk) + "\n\n"
+            chunk = re_sub(r'<br />\s*<br />', "\n\n", chunk)
             # Space things out a little
-            chunk = sub('(<' + block  + '[^>]*>)', "\n\1", chunk)
+            chunk = re_sub('(<' + block  + '[^>]*>)', "\n\\1", chunk)
             # Space things out a little
-            chunk = sub('(</' + $block + '>)', "\1\n\n", chunk)
+            chunk = re_sub('(</' + block + '>)', "\\1\n\n", chunk)
             # take care of duplicates
-            chunk = sub("\n\n+", "\n\n", chunk)
-            chunk = sub('^\n|\n\s*\n$', '', chunk)
+            chunk = re_sub(r"\n\n+", "\n\n", chunk)
+            chunk = re_sub(r'^\n|\n\s*\n$', '', chunk)
             # make paragraphs, including one at the end
-            chunk = '<p>' + sub('\n\s*\n\n?(.)', "</p>\n<p>\1", chunk) \
+            chunk = '<p>' + re_sub(r'\n\s*\n\n?(.)', "</p>\n<p>\\1", chunk) \
                     + "</p>\n";
             # problem with nested lists
-            chunk = sub("<p>(<li.+?)</p>", "\1", chunk);
-            chunk = sub('<p><blockquote([^>]*)>', "<blockquote\1><p>",
-                        chunk, flags=I)
+            chunk = re_sub("<p>(<li.+?)</p>", "\\1", chunk);
+            chunk = re_sub('<p><blockquote([^>]*)>', "<blockquote\\1><p>",
+                        chunk, flags=re.I)
             chunk = chunk.replace('</blockquote></p>', '</p></blockquote>')
             # under certain strange conditions it could
             # create a P of entirely whitespace
-            chunk = sub('<p>\s*</p>\n?', '', chunk)
-            chunk = sub('<p>\s*(</?'+ block + '[^>]*>)', "\1", chunk)
-            chunk = sub('(</?' + block + '[^>]*>)\s*</p>', "\1", chunk)
+            chunk = re_sub(r'<p>\s*</p>\n?', '', chunk)
+            chunk = re_sub(r'<p>\s*(</?'+ block + '[^>]*>)', "\\1", chunk)
+            chunk = re_sub(r'(</?' + block + '[^>]*>)\s*</p>', "\\1", chunk)
             # make line breaks
-            chunk = sub('(?<!<br />)\s*\n', "<br />\n", chunk) 
-            chunk = sub('(</?' + $block + '[^>]*>)\s*<br />', "\1", chunk)
-            chunk = sub('<br />(\s*</?(?:p|li|div|th|pre|td|ul|ol)>)', '\1',
-                        chunk)
-            chunk = sub('&([^#])(?![A-Za-z0-9]{1,8};)', '&amp;\1', chunk);
+            chunk = re_sub(r'(?<!<br />)\s*\n', "<br />\n", chunk) 
+            chunk = re_sub('(</?' + block + '[^>]*>)\s*<br />', "\\1", chunk)
+            chunk = re_sub('<br />(\s*</?(?:p|li|div|th|pre|td|ul|ol)>)',
+                           '\\1', chunk)
+            chunk = re_sub('&([^#])(?![A-Za-z0-9]{1,8};)', '&amp;\\1', chunk)
         output += chunk
     return output
--- re_memo.py
+++ re_memo.py
@@ -14,22 +14,18 @@ import re
 re_exprs = dict()
 
 # Function for memoizing a thing.
-def memoize_re(expr):
+def memoize_re(expr, flags=0):
     global re_exprs
     if expr in re_exprs:
         return re_exprs[expr]
-    rc = re.compile(expr)
+    rc = re.compile(expr, flags=flags)
     re_exprs[expr] = rc
     return rc
     
 # Memoized split.
-def re_split(expr, *args):
-    return memoize_re(expr).split(*args)
+def re_split(expr, *args, maxsplit=0, flags=0):
+    return memoize_re(expr, flags=flags).split(*args, maxsplit=maxsplit)
 
 # Memoized sub.
-def re_sub(expr, sub, value):
-    return memoize_re(expr).sub(sub, value)
-
-# Memoized search.
-def re_search(expr, value):
-    return memoize_re(expr).search(value)
+def re_sub(expr, *args, count=0, flags=0):
+    return memoize_re(expr, flags=flags).sub(*args, count=count)
